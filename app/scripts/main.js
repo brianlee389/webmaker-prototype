45770082bcf89651f5bd313773b3d0d77762699f
Brian Lee <bl389@drexel.edu>
2015-04-07 17:04:05 -0400

    Working on the expanding the list item to show options for editing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -19,22 +19,74 @@ $(function() {
         placeholder: "Select a HTML tag"
     });
     
+    $('.edit-tag').simpleexpand();
+    
     function createTag(tagname) {
         return "< " + tagname + " >"
     }
     
+    // a lot more abstractions can be made, but this is a prototype
+    // so I didn't care
     $(".create").click(function() {
         var selectVal = $(".select-tags").val();
-        var insidediv = $('<div></div>').addClass('dd-handle mobile-li').text(createTag(selectVal));
+        var insidediv = $('<div></div>').addClass('mobile-li');
+        
+        var spanleft = $('<span></span>').addClass('dd-handle').text(createTag(selectVal));
+        var spanright = $('<span></span>').addClass('edit-tag').text('Edit');
+        
+        insidediv.append(spanleft);
+        insidediv.append(spanright);
+        
         var newid = $('.dd-item').size() + 1;
         var litag = $('<li></li>').addClass("dd-item").attr("data-id", newid).append(insidediv);
 
+        var expandableContent = $("<div></div>").addClass("content mobile-li");
+        
+        var button = $('<button></button>').addClass('circle-button');
+        var buttonText = button.clone().text("text");
+        var buttonLink = button.clone().text("link");
+        
+        expandableContent.append(buttonLink);
+        expandableContent.append(buttonText);
+        
         var ddlist = $('.dd').find('.dd-list').first();
         ddlist.append(litag).fadeIn();
+        
+        // setup the expand
+        $(spanright).simpleexpand();
     });
     
+
+                        
+                        
     
     /* Rendering the preview */
+    var previewTag = $(".preview").children(".scrollable");
+    var initialList = $('.dd').children(".scrollable").children(".dd-list");
+    
+    function renderList(addto, additem) {
+        var itemlist = additem.children(".dd-item");
+        var listsize = itemlist.length;
+        for(var i = 0; i < itemsize; i++) {
+            var item = itemlist[i];
+            var data = item.find(".dd-handle").first();
+            //create the tag from the data
+            
+            var innerlist = item.find(".dd-list");
+            if(innerlist.length > 0) {
+                // pass the created tag to renderList and add the rest of the inner tags
+                //renderList(innerlist)
+            }
+        }
+    }
+    
+    $( "#accordion" ).accordion({
+        collapsible: true
+    });
+    // renderList(previewTag, initialList)
+    
+    
+    //.children("li")
 });
 
 
