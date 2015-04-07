9c71178400d9a9c6e18a816492cb3017b14aa128
Brian Lee <bl389@drexel.edu>
2015-04-07 14:05:19 -0400

    This commit includes all the needed styles and scripts, the earlier commits will not work. Added popup modal for adding tags and the feature to add new list items that should represent tags.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,23 +1,40 @@
-$(".add-tag-button").click(function() {
-    var insidediv = $('<div></div>').addClass('dd-handle mobile-li').text("<a>");
-    var newid = $('.dd-item').size() + 1;
-    var litag = $('<li></li>').addClass("dd-item").attr("data-id", newid).append(insidediv);
+$(function() {
+    $('.dd').nestable();
+
+    // custombox modal
     
-    var ddlist = $('.dd').find('.dd-list').first();
-    ddlist.append(litag).fadeIn();
+    // attach close button handler
+    $('.modal .close').on('click', function(e){
+        e.preventDefault();
+        $.modal().close();
+    });
+
+    // open modal with default options or options set with init
+    // content will be taken from #login
+    $(".add-tag-button").click(function() {
+        $('.modal').modal().open();
+    });
+    
+    $(".select-tags").selectOrDie({
+        placeholder: "Select a HTML tag"
+    });
+    
+    function createTag(tagname) {
+        return "< " + tagname + " >"
+    }
+    
+    $(".create").click(function() {
+        var selectVal = $(".select-tags").val();
+        var insidediv = $('<div></div>').addClass('dd-handle mobile-li').text(createTag(selectVal));
+        var newid = $('.dd-item').size() + 1;
+        var litag = $('<li></li>').addClass("dd-item").attr("data-id", newid).append(insidediv);
+
+        var ddlist = $('.dd').find('.dd-list').first();
+        ddlist.append(litag).fadeIn();
+    });
+    
+    
+    /* Rendering the preview */
 });
 
 
-$('.dd').nestable();
-
-
-/*
-console.log($('.dd-item').size());
-$('<li></li>').addClass("dd-item");
-$('<div></div>').addClass('dd-handle');
-*/
-/*
-
-<li class="dd-item" data-id="1">
-    <div class="dd-handle mobile-li">&lt; a &gt;</div>
-</li>*/
