f1ab75c9f7efcc423e498ea72f9f54e8c718d264
Brian Lee <bl389@drexel.edu>
2015-04-07 12:47:04 -0400

    Cleaned up the html and css. Nestable drag and drop is implemented.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,23 @@
-$(".add-list-item").click(function() {
+$(".add-tag-button").click(function() {
+    var insidediv = $('<div></div>').addClass('dd-handle mobile-li').text("<a>");
+    var newid = $('.dd-item').size() + 1;
+    var litag = $('<li></li>').addClass("dd-item").attr("data-id", newid).append(insidediv);
     
+    var ddlist = $('.dd').find('.dd-list').first();
+    ddlist.append(litag).fadeIn();
 });
 
-$(".add-list-item").mousedown(function() {
-    $(this).css("opacity", "0.5");
-}).mouseup(function(){
-    $(this).css("opacity", "1");
-
-});
 
 $('.dd').nestable();
+
+
+/*
+console.log($('.dd-item').size());
+$('<li></li>').addClass("dd-item");
+$('<div></div>').addClass('dd-handle');
+*/
+/*
+
+<li class="dd-item" data-id="1">
+    <div class="dd-handle mobile-li">&lt; a &gt;</div>
+</li>*/
